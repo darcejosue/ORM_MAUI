5d8c0905119e20cd1a3af69fcb71ee0015b139a2
Josue M Darce <133271041+darcejosue@users.noreply.github.com>
2024-11-05 13:59:57 -0600

    listo la visualizacion

--- ORM/Pages/Inventario.xaml.cs
+++ ORM/Pages/Inventario.xaml.cs
@@ -1,11 +1,13 @@
 using ORM.Componentes;
 using ORM.Models;
+using System.Collections.ObjectModel;
 
 namespace ORM.Pages;
 
 public partial class Inventario : ContentPage
 {
-	private Item _currentItem;
+	private ObservableCollection<ProductoModel> _products;
+	private ObservableCollection<ProductoModel> _filteredProducts;
 	public Inventario()
 	{
 		InitializeComponent();
@@ -14,11 +16,27 @@ public partial class Inventario : ContentPage
 	protected override async void OnAppearing()
 	{
 		base.OnAppearing();
-        ItemsListView.ItemsSource = await App.ProductoBase.GetProducts();
+        //ItemsListView.ItemsSource = await App.ProductoBase.GetProducts();
+        var productos = await App.ProductoBase.GetProducts();
+		_products = new ObservableCollection<ProductoModel>(productos);
+		_filteredProducts = new ObservableCollection<ProductoModel>(_products);
+        ItemsListView.ItemsSource = _filteredProducts.Take(25).ToList();
+
+    }
+
+	private void OnSearchBar(object sender, TextChangedEventArgs e)
+	{
+		var searchBar = e.NewTextValue;
+		_filteredProducts.Clear();
+		var filterItems = _products.Where(p => p.Nombre.ToLower().Contains(searchBar.ToLower()));
+		foreach (var item in filterItems)
+		{
+			_filteredProducts.Add(item);
+		}
 	}
-    private void OnEditItemClicked(object sender, EventArgs e) {
-		
-	}
+    private async void OnEditItemClicked(object sender, EventArgs e) {
+        await Navigation.PushAsync(new NuevoInventario());
+    }
 
 	private async void OnDeleteItemClicked(object sender, EventArgs e)
 	{
